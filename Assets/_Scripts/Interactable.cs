aa5de39906e3ee63a9b7d6fb1674af314e5f712c
Jaskanwar <57963331+Jaskanwar@users.noreply.github.com>
2020-03-17 13:34:05 -0400

    Delete .gitattributes

---
+++ Assets/_Scripts/Interactable.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    bool isFocus = false;
+
+    bool hasInteracted = false;
+
+    Transform player;
+
+    public Transform interactionTransform;
+
+
+    public virtual void Interact()
+    {
+        
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Camera cam;
+    public LayerMask movementMask;
+    public PlayerMotor motor;
+    public Interactable focus;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+                // Move our player to what we hit
+                // Stop focusing any objects
+                motor.MoveToPoint(hit.point);
+                RemoveFocus();
+            }
+
+           
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Check if we hit an interactable, if we did do something.
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable!= null)
+                {
+                    SetFocus(interactable);
+                }
+
+            }
+        }
+
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus!= focus)
+        {
+            if(focus != null)
+                focus.OnDefocused();
+            
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+           
+        }
+        newFocus.OnFocused(transform);
+
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+        
+        
+        focus = null;
+        motor.StopFollowingTarget();
+
+    }
+}
